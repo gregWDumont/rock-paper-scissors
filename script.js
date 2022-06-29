db583f8d8ed794a831dbe6c2083b046e0a6edc76
Reggr0y <gregory.wd@pm.me>
2022-06-29 18:41:36 -0400

    Reorder the files and add the first function in script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let playerSelection;
+let computerSelection;
+
+function playRound(playerSelection, computerSelection) {
+    
+  }
+
+const myArray = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+    return myArray[Math.floor(Math.random() * myArray.length)];
+}
+console.log(computerPlay());
