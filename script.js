7826c72741f14bbf2931361143020d29482fb9ff
Reggr0y <gregory.wd@pm.me>
2022-06-29 19:00:14 -0400

    Integrate conditional rules for winning and scores

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let playerSelection;
+let playerScore = 0;
 let computerSelection;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (
+    (playerSelection === 'Paper') && (computerSelection === 'Rock') || 
+    (playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+    (playerSelection === 'Rock' && computerSelection === 'Scissors')
+    ) {
+        playerScore++
+        roundWinner = userName
+    }
+
+    else if (playerSelection === computerSelection) {
+        roundWinner = 'None'
+    }
+    else { 
+        computerScore++
+        roundWinner = 'Robot'
+    }
+    updateScoreMessage(roundWinner, playerSelection, computerSelection);
   }
 
 const myArray = ["Rock", "Paper", "Scissors"];
