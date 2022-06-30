1246ed3ae16b212ae12038949d05129b43464157
Reggr0y <gregory.wd@pm.me>
2022-06-30 15:05:12 -0500

    Ask user to give name and display personalise message

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+//User name 
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h2');
+
+function setUserName() {
+let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = ("It's you against the machines!" + "\n" + "Go on " + myName + "!");
+    }
+}
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = ("Hello back " + storedName + ", it's still you against the machines." + "\n" + "Go get them!");
+  }
+  
 let playerSelection;
-let playerScore = 0;
 let computerSelection;
+let playerScore = 0;
 let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
