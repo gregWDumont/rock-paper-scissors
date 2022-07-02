e1c91eeaa71748070e1f855b97c11c881876ea77
Reggr0y <gregory.wd@pm.me>
2022-07-02 10:45:47 -0500

    Try to simplify the code to highlight what is blocking without success

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //User name request and text update
-
+/*
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
@@ -48,9 +48,9 @@ btn.addEventListener('click', function(){
     setTimeout(() => {  typeWriter(); }, 1000);
 // Add event listener to all of the game buttons
     gameButtons = document.querySelectorAll(".gameselection").forEach(item => {
-        item.addEventListener("click", playSound);
+        item.addEventListener("click");
     })
-});
+}); */
   
 // Game events
 // Computer and user score
@@ -64,18 +64,18 @@ function playRound(playerSelection, computerSelection) {
     if (
     (playerSelection === 'Paper') && (computerSelection === 'Rock') || 
     (playerSelection === 'Rock' && computerSelection === 'Scissors') || 
-    (playerSelection === 'Rock' && computerSelection === 'Scissors')
+    (playerSelection === 'Scissors' && computerSelection === 'Paper')
     ) {
         playerScore++
-        roundWinner = userName
+        roundWinner = 'player'
     }
 
     else if (playerSelection === computerSelection) {
-        roundWinner = 'None'
+        roundWinner = 'none'
     }
     else { 
         computerScore++
-        roundWinner = 'Robot'
+        roundWinner = 'robot'
     }
     updateScoreMessage(roundWinner, playerSelection, computerSelection);
   }
@@ -89,10 +89,62 @@ function computerPlay() {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-// game over
+// Game over
 
   function isGameOver() {
     return playerScore === 5 || computerScore === 5
   }
 
-console.log(computerPlay());
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockClick = document.getElementById('#Rock')
+const paperClick = document.getElementById('#Paper')
+const scissorsClick = document.getElementById('#Scissors')
+const rockRobClick = document.getElementById('#RobRock')
+const paperRobClick = document.getElementById('#RobPaper')
+const scissorsRobClick = document.getElementById('#RobScissors')
+
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
+
+rockClick.addEventListener('click', () => handleClick('#Rock'))
+paperClick.addEventListener('click', () => handleClick('#Paper'))
+scissorsClick.addEventListener('click', () => handleClick('#Scissors'))
+restartBtn.addEventListener('click', restartGame)
+overlay.addEventListener('click', closeEndgameModal)
+
+function handleClick(playerSelection) {
+    if (isGameOver()) {
+      openEndgameModal()
+      return
+    }
+  
+    const computerSelection = computerPlay()
+    playRound(playerSelection, computerSelection)
+    updateChoices(playerSelection, computerSelection)
+    updateScore()
+  
+    if (isGameOver()) {
+      openEndgameModal()
+      setFinalMessage()
+    }
+  }
+  
+  function updateScore() {
+    if (roundWinner === 'none') {
+      scoreInfo.textContent = "It's a tie!"
+    } else if (roundWinner === 'player') {
+      scoreInfo.textContent = 'You won!'
+    } else if (roundWinner === 'robot') {
+      scoreInfo.textContent = 'You lost!'
+    }
+  
+    playerScorePara.textContent = `Player: ${playerScore}`
+    computerScorePara.textContent = `Computer: ${computerScore}`
+  }
