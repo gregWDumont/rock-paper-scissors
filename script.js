33c45d04a5bfed8c5d688f0cf2fad514c38ac67e
Reggr0y <gregory.wd@pm.me>
2022-07-03 18:57:31 -0500

    Ask username and associate it with player info and game results

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
+//function to randomly 
+
 const weapArray = ["rock", "paper", "scissors"];
 
 function computerPlay() {
     return weapArray[Math.floor(Math.random() * 3)];
 }
 
-let playerSelection = ""
-let computerSelection = ""
+//Ask user name
+
+    let userName = prompt("Enter you name: ");
+    if (userName != null) { 
+        document.getElementById("player-name").innerText = userName;      
+    };
+
+
+let computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
 
 
 function playRound(playerSelection, computerSelection) 
 {
+    let winner = "";
     let infoResult = "";
     if (playerSelection === computerSelection) {
             winner = none;
-            infoResult = ("It's a tie");
+            infoResult = "It's a tie";
     }
     else if (
         (playerSelection == 'rock') && (computerSelection == 'paper') ||
         (playerSelection == 'paper') && (computerSelection == 'scissors') ||
         (playerSelection == 'scissors') && (computerSelection == 'paper')
         ) {
+            computerScore++;
             winner = 'computer';
-            infoResult = 'You lose, ' + computerSelection + ' beats ' + playerSelection;
+            infoResult = 'You lose, ' + playerSelection + ' beats ' + computerSelection;
             }
     else {
-        winner = 'user';
+        playerScore++;
+        winner = userName;
         infoResult = 'You win, ' + playerSelection + ' beats ' + computerSelection;
     };
+    document.getElementById('winner').innerHTML = winner;
+    document.getElementById('message').innerHTML = infoResult;
     return infoResult;
 }
 
-console.log(playRound(rock, computerSelection));
+console.log(playRound('rock', computerSelection));
